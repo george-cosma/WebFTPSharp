bf7655047393b66e7bd7670586a69327f7d61250
George Cosma <georgemagic10@gmail.com>
2022-05-08 00:54:53 +0300

    Initial

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebFTPSharp.Pages
+{
+	public class IndexModel : PageModel
+	{
+		private readonly ILogger<IndexModel> _logger;
+
+		public IndexModel(ILogger<IndexModel> logger)
+		{
+			_logger = logger;
+		}
+
+		public void OnGet()
+		{
+			DynamicString = "Hello World";
+
+		}
+
+		public string DynamicString { get; set; }
+	}
+}
---
+++ wwwroot/js/filebrowser.js
@@ -0,0 +1,23 @@
+﻿var connection = new signalR.HubConnectionBuilder().withUrl("/api/hubs/filebrowser").build();
+connection.start();
+
+/*
+
+connection.on("ReceiveMessage", function (message) {
+    var li = document.createElement("li");
+    document.getElementById("messagesList").appendChild(li);
+    // We can assign user-supplied strings to an element's textContent because it
+    // is not interpreted as markup. If you're assigning in any other way, you 
+    // should be aware of possible script injection concerns.
+    li.textContent = `${message}`;
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var message = document.getElementById("messageInput").value;
+    connection.invoke("SendMessage", message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
+
+*/
