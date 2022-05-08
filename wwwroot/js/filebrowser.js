b5298e178649b3fd14f716339461027a97eb8e93
George Cosma <georgemagic10@gmail.com>
2022-05-08 12:13:45 +0300

    fixed project thinking javascript files were typescript

--- wwwroot/js/filebrowser.js
+++ wwwroot/js/filebrowser.js
@@ -11,7 +11,7 @@ connection.on("ReceiveMessage", function (message) {
     var li = document.createElement("li");
     document.getElementById("messagesList").appendChild(li);
     // We can assign user-supplied strings to an element's textContent because it
-    // is not interpreted as markup. If you're assigning in any other way, you 
+    // is not interpreted as markup. If you're assigning in any other way, you
     // should be aware of possible script injection concerns.
     li.textContent = `${message}`;
 });
